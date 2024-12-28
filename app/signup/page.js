5865e58e6be1d112a94e5ca9e86895f2b456863d
Debmalya De Sarkar <debmalya.desarkar@gmail.com>
2024-12-28 20:38:20 +0530

    firebase rout fixed

--- app/signup/page.js
+++ app/signup/page.js
@@ -1,7 +1,7 @@
 "use client"
 import React, { useState } from 'react';
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from '../lib/firebase';
+import { auth } from "@/lib/firebase";
 
 
 const LoginPage = () => {
