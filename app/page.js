4c9bd17cc817535996efe16ae99b0e2b51955371
Debmalya De Sarkar <debmalya.desarkar@gmail.com>
2025-01-05 22:22:03 +0530

    base screen share implemented

--- app/page.js
+++ app/page.js
@@ -1,35 +1,25 @@
 "use client";
-import {
-    useContext,
-    useEffect,
-    useLayoutEffect,
-    useRef,
-    useState,
-} from "react";
+import { useContext, useEffect, useState } from "react";
 import { RoomContext } from "@/context/roomContext";
 import Peer from "peerjs";
 import { v4 as uuid4 } from "uuid";
 import VideoPlayer from "@/components/VideoPlayer";
+import { metadata } from "./layout";
 
 export default function Home() {
     const { io } = useContext(RoomContext);
-
     const [roomId, setRoomId] = useState(null);
+
     const [localPeer, setLocalPeer] = useState(null);
+
+    const [localScreenSharePeer, setLocalScreenSharePeer] = useState(null);
+
     const [inputRoomId, setInputRoomId] = useState(null);
-    /*
-     *  remoteMembers: {peerId: stream}
-     */
     const [remoteMembers, setRemoteMembers] = useState({});
     const [localStream, setLocalStream] = useState(null);
 
-    // handeling join room
     const handelJoin = ({ roomId }) => {
-        console.log("req to join with id: ", roomId);
-
         if (localPeer) {
-            console.log("this is peer id form me: ", localPeer._id);
-
             try {
                 inputRoomId
                     ? io.emit("join-room", {
@@ -43,53 +33,35 @@ export default function Home() {
         }
     };
 
-    // Request to create a room
     const hadelCreateRoom = () => {
-        console.log("req to create room");
         io.emit("create-room");
     };
 
-    // handelin input on change
     const handelInput = ({ value }) => {
-        console.log("room id manual: ", value);
         setInputRoomId(value);
     };
 
-
-    /**
-     * Listening to
-     * 1. room-create
-     * 2. get-users
-     * 3. error at socket
-     *
-     * Generating new peer for local users
-     */
     useEffect(() => {
-        // On room created
         io.on("room-cretaed", (data) => {
-            console.log("data from room created: ", data);
             setRoomId(data.roomId);
         });
-    
 
-        // For any error at socket
         io.on("error", (error) => {
             console.log("error at socket:", error);
         });
 
-        // Generating peer instance and assiginging id for the user
+        // replace this firebase uid
         const myId = uuid4();
-
         const peer = new Peer(myId);
 
-        console.log("this is peer: ", peer);
-
+        // Setting up the local peer
         setLocalPeer(peer);
+
+        // setting up the screen share peer
+        const screenSharePeer = new Peer(`${myId}-screen-share`);
+        setLocalScreenSharePeer(screenSharePeer);
     }, []);
 
-    /**
-     *  Handeling audio and video stream
-     */
     useEffect(() => {
         (async () => {
             try {
@@ -99,107 +71,95 @@ export default function Home() {
                 });
 
                 setLocalStream(stream);
-               
             } catch (e) {
                 console.log("error at getting media: ", e);
             }
         })();
     }, [localPeer]);
 
-
-    // handeling calls
-    useEffect(() => {
-        //calling user who have joined and passing the stream
-        if (localStream) {
-            try {
-                io.on("user-joined", ({ peerId }) => {
-                    console.log("remote join user", peerId);
-
-                    if (localPeer) {
-                       
-                        const call = localPeer.call(peerId, localStream);
-
-                        // While getting remote user stream assign to remote video
-                        call.on("stream", (peerStream) => {
-                            console.log("call peer call: ", { peerStream });
-                            setRemoteMembers(prevState => ({
-                                ...prevState,
-                                [peerId]: peerStream,
-                            }));
-                            // remoteVideoRef.current.srcObject = peerStream;
-                        });
-                    }
-                });
-            } catch (e) {
-                console.log(e);
-            }
-        }
+    // Handle screen share
+    const startScreenShare = async () => {
+        const screenStream = await navigator.mediaDevices.getDisplayMedia({
+            video: true,
+        });
 
         
-        if (localPeer) {
+
+        // Share screen with all the remote members
+        Object.keys(remoteMembers).forEach((peerId) => {
             try {
-                localPeer.on("call", (call) => {
-                    console.log("call form remote: ", call);
-                    call.answer(localStream);
+                const call = localScreenSharePeer.call(peerId, screenStream);
+                call.on("error", (err) => {
+                    console.error(
+                        `Error during screen share call with ${peerId}:`,
+                        err
+                    );
+                });
+            } catch (err) {
+                console.error(
+                    `Failed to call ${peerId} for screen sharing:`,
+                    err
+                );
+            }
+        });
+    };
+
+    useEffect(() => {
+        if (localStream) {
+            io.on("user-joined", ({ peerId }) => {
+                if (localPeer) {
+                    const call = localPeer.call(
+                        peerId, 
+                        localStream,
+                    );
                     call.on("stream", (peerStream) => {
-                        console.log("peer stream: ", peerStream);
-                        setRemoteMembers(prevState => ({
+                        setRemoteMembers((prevState) => ({
                             ...prevState,
-                            [call.peer]: peerStream,
+                            [peerId]: peerStream,
                         }));
                     });
+                }
+            });
+        }
+
+        if (localPeer) {
+            localPeer.on("call", (call) => {
+                call.answer(localStream);
+
+                call.on("stream", (peerStream) => {
+                    setRemoteMembers((prevState) => ({
+                        ...prevState,
+                        [call.peer]: peerStream,
+                    }));
                 });
-            } catch (e) {
-                console.log("error at answering: ", e);
-            }
+            });
         }
 
         io.on("user-disconnected", ({ peerId }) => {
-            console.log("user disconnected", peerId);
             setRemoteMembers((prevState) => {
                 const newState = { ...prevState };
                 delete newState[peerId];
                 return newState;
             });
         });
-
-
     }, [localPeer, localStream]);
 
     return (
-        console.log("remoteMembers", remoteMembers),
         <>
             <div>roomID {roomId}</div>
             <input
                 type="text"
                 onChange={(e) => handelInput({ value: e.target.value })}
             />
-            <button
-                onClick={() => {
-                    hadelCreateRoom();
-                }}
-            >
-                Create Room
-            </button>
-            <button
-                onClick={() => {
-                    handelJoin({ roomId });
-                }}
-            >
-                join room
-            </button>
+            <button onClick={hadelCreateRoom}>Create Room</button>
+            <button onClick={() => handelJoin({ roomId })}>Join Room</button>
+            <button onClick={startScreenShare}>Share Screen</button>
 
             {/* Video interface */}
             {localStream && <VideoPlayer stream={localStream} muted />}
-
-            {Object.keys(remoteMembers).map((peerId, index) => {
-                return (
-                    <VideoPlayer
-                        key={index}
-                        stream={remoteMembers[peerId]}
-                    />
-                );
-            })}
+            {Object.keys(remoteMembers).map((peerId, index) => (
+                <VideoPlayer key={index} stream={remoteMembers[peerId]} />
+            ))}
         </>
     );
 }
